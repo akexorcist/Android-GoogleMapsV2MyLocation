a3c05335342d43834020aeb58efa9944bf23cdd9
akexorcist <akexorcist@gmail.com>
2014-06-07 16:08:38 +0700

    ..

---
+++ gen/app/akexorcist/googlemapsv2mylocation/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.akexorcist.googlemapsv2mylocation;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
